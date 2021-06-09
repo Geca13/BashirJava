0e87c71eadf138912015071d1f0041f2e1c3e02b
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-06-09 08:20:06 +0200

    first commit

---
+++ src/main/java/com/example/bashir/BashirApplication.java
@@ -0,0 +1,13 @@
+package com.example.bashir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BashirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BashirApplication.class, args);
+	}
+
+}
