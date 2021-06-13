888ecd5beddd4f60bf5d3564512159cc8181c218
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-06-14 00:05:29 +0200

    started with testing with basic security

--- src/main/java/com/example/bashir/user/UserController.java
+++ src/main/java/com/example/bashir/user/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ import com.example.bashir.shared.GenericResponse;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 	
 	@Autowired
